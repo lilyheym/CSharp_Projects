4d43913bef031f2509c98732ab5e7e6439a58c0c
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-12-13 15:10:24 -0800

    fixed comments

--- ConsoleMathApp/ConsoleMathApp/Program.cs
+++ ConsoleMathApp/ConsoleMathApp/Program.cs
@@ -10,42 +10,54 @@ namespace ConsoleMathApp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Please add a number between 0 and 10,000,000");
-            //string timesFifty = Console.ReadLine();
-            //int multByFifty = Convert.ToInt32(timesFifty);
-            //int resultFifty = multByFifty * 50;
-            //Console.WriteLine(multByFifty + " multiplied by 50 equals " + resultFifty + "!");
-            //Console.ReadLine();
 
-            //Console.WriteLine("Please write another number");
-            //string addTwentyFive = Console.ReadLine();
-            //int add25 = Convert.ToInt32(addTwentyFive);
-            //int addingResult = add25 + 25;
-            //Console.WriteLine(add25 + " plus 25 equals " + addingResult + "!");
-            //Console.ReadLine();
+            // multiply by 50
 
-            //Console.WriteLine("Please write another number");
-            //string divideBy = Console.ReadLine();
-            //double divByTwelve = Convert.ToDouble(divideBy);
-            //double divQuotient = divByTwelve / 12.5;
-            //Console.WriteLine(divByTwelve + " divided by 12.5 equals " + divQuotient + "!");
-            //Console.ReadLine();
+            Console.WriteLine("Please add a number between 0 and 10,000,000");
+            string timesFifty = Console.ReadLine();
+            int multByFifty = Convert.ToInt32(timesFifty);
+            int resultFifty = multByFifty * 50;
+            Console.WriteLine(multByFifty + " multiplied by 50 equals " + resultFifty + "!");
+            Console.ReadLine();
 
-            //Console.WriteLine("Please write another number, we're going to check if it's greater than 50");
-            //string greaterThan = Console.ReadLine();
-            //int greaterFifty = Convert.ToInt32(greaterThan);
-            //int fifty = 50;
-            //bool testFifty = greaterFifty > fifty;
-            //if (testFifty)
-            //{
-            //    Console.WriteLine("true");
-            //    Console.ReadLine();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("false");
-            //    Console.ReadLine();
-            //}
+            // add 25
+
+            Console.WriteLine("Please write another number");
+            string addTwentyFive = Console.ReadLine();
+            int add25 = Convert.ToInt32(addTwentyFive);
+            int addingResult = add25 + 25;
+            Console.WriteLine(add25 + " plus 25 equals " + addingResult + "!");
+            Console.ReadLine();
+
+            // divide by 12.5
+
+            Console.WriteLine("Please write another number");
+            string divideBy = Console.ReadLine();
+            double divByTwelve = Convert.ToDouble(divideBy);
+            double divQuotient = divByTwelve / 12.5;
+            Console.WriteLine(divByTwelve + " divided by 12.5 equals " + divQuotient + "!");
+            Console.ReadLine();
+
+            // greater than 50
+
+            Console.WriteLine("Please write another number, we're going to check if it's greater than 50");
+            string greaterThan = Console.ReadLine();
+            int greaterFifty = Convert.ToInt32(greaterThan);
+            int fifty = 50;
+            bool testFifty = greaterFifty > fifty;
+
+            if (testFifty)
+            {
+                Console.WriteLine("true");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("false");
+                Console.ReadLine();
+            }
+
+            // find remainder
 
             Console.WriteLine("Please write another number, we are going to divide it by 7 and give you the remainder");
             string giveRemainder = Console.ReadLine();
