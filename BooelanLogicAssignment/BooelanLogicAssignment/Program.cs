1f1cc179577d816de5cb7b6de58010ffc11b8a70
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-12-14 14:06:36 -0800

    comments

--- BooelanLogicAssignment/BooelanLogicAssignment/Program.cs
+++ BooelanLogicAssignment/BooelanLogicAssignment/Program.cs
@@ -13,17 +13,23 @@ namespace BooelanLogicAssignment
             Console.WriteLine("Car Insurance Approval Questionare");
             Console.ReadLine();
 
+            // Age over 15
+
             Console.WriteLine("What is your age?");
             string ageString = Console.ReadLine();
             int ageInt = Convert.ToInt32(ageString);
             bool ageFifteen = ageInt > 15;
             
 
+            // No DUIs
+
             Console.WriteLine("Have you ever had a DUI?");
             string dui = Console.ReadLine();
             bool duiHowMany = Convert.ToBoolean(dui);
             bool duiQualify = duiHowMany == false;
 
+
+            // Under 3 sppeding tickets
 
             Console.WriteLine("How many speeding tickets do you have?");
             string speeding = Console.ReadLine();
