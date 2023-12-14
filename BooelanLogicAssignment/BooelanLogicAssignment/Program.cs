ae7422f12083dc61ce1665b2a439a5e1310a6d43
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-12-14 14:05:17 -0800

    Boolean Logic Assignment

---
+++ BooelanLogicAssignment/BooelanLogicAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooelanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Approval Questionare");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string ageString = Console.ReadLine();
+            int ageInt = Convert.ToInt32(ageString);
+            bool ageFifteen = ageInt > 15;
+            
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            bool duiHowMany = Convert.ToBoolean(dui);
+            bool duiQualify = duiHowMany == false;
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speeding = Console.ReadLine();
+            int speedingTicket = Convert.ToInt32(speeding);
+            bool ticketSpeeding = speedingTicket < 3;
+
+            Console.WriteLine("Qualified?");
+            Console.ReadLine();
+            Console.WriteLine(ageFifteen && duiQualify && ticketSpeeding);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
