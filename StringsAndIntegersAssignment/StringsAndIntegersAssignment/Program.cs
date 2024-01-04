ae26eb5fd9a58e292bc0c853f8a84d5f87f83ee9
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-04 01:37:42 -0800

    StringsAndIntegersAssignment

---
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listNumberFour = new List<int> { 1, 45, 38, 92, 90, 4, 76, 84, 33, 11, 32, 470, 857 };
+
+            Console.WriteLine("Please enter a number.");
+            int bill = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int frank in listNumberFour)
+            {
+                int wanda = frank / bill;
+                Console.WriteLine(wanda);
+            }
+            Console.ReadLine();
+        }
+    }
+}
