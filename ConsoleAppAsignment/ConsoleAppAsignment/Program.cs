44a6875dcb17fb31bd5d90477cf3c20e3807361a
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-03 16:24:54 -0800

    Console Assignment

--- ConsoleAppAsignment/ConsoleAppAsignment/Program.cs
+++ ConsoleAppAsignment/ConsoleAppAsignment/Program.cs
@@ -10,32 +10,146 @@ namespace ConsoleAppAsignment
     {
         static void Main(string[] args)
         {
-            // Part One: One-Dimensional array of Strings
+            // Part One
 
-            string[] numArray = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
-            
+            // string[] numArray = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
 
-            // User Input
 
-            Console.WriteLine("Please write something below:");
-            string numArrayUser = Console.ReadLine();
+            // // User Input
 
-            // Loop One
+            // Console.WriteLine("Please write something below:");
+            // string numArrayUser = Console.ReadLine();
 
-           foreach (string number in numArray)
-            {
-                numArray = numArray.Select((Y) => { return Y + numArrayUser; }).ToArray();
-            }
+            // // did one loop instead of two, thank you instructors
 
-            // Loop Two
+            //for ( int i = 0; i < numArray.Length; i++)
+            // {
+            //     numArray[i] += numArrayUser;
+            //     Console.WriteLine(numArray[i]);
+            // }
+            // Console.ReadLine();
 
-            for ( int i = 0; i < numArray.Length; i++)
-            {
-                Console.WriteLine(numArray[i]);
-            }
-            Console.ReadLine();
+            // Part Two
 
-            // Part One End
+            //var x = true;
+
+            //while (x)
+            //{
+            //    Console.WriteLine("Keep the change, ya filthy animal!");
+            //    break;   // Inifinite Loop fixed by adding break
+            //}
+            //Console.ReadLine();
+
+            // Part Three
+
+            // less than operator
+
+            //Console.WriteLine("Please enter a number.");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //if (userInput < 25)
+            //{
+            //    Console.WriteLine("This number is not too old for Leonardo DiCaprio.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This number is too old for Leonardo DiCaprio.");
+            //}
+            //Console.ReadLine();
+
+            // less than or equals to operator
+
+            //Console.WriteLine("Please enter another number (no more DiCaprio jokes I promise).");
+            //int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (userInput2 <= 25)
+            //{
+            //    Console.WriteLine("This number is less than or equal to 25");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This number is larger than 25");
+            //}
+            //Console.ReadLine();
+
+            // Part Four
+
+            //List<string> fruit = new List<string>
+            //{
+            //    "apple",
+            //    "banana",
+            //    "watermelon",
+            //    "pomegranate",
+            //    "orange",
+            //    "mango",
+            //    "peach",
+            //    "plum",
+            //    "pear",
+            //    "lychee",
+            //    "strawberry",
+            //    "blueberry",
+            //    "grapefuit",
+            //    "cantelope",
+            //    "cherry",
+            //    "pineapple",
+            //    "grape",
+            //};
+
+            //Console.WriteLine("Please enter a fruit (lower case).");
+            //string fruitInput = Console.ReadLine();
+
+            //if (fruit.Contains(fruitInput))
+            //{
+            //    Console.WriteLine(fruit.IndexOf(fruitInput));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, that fruit is not in the catelog.");
+            //}
+            //Console.ReadLine();
+
+            // Part Five
+
+            //List<string> colors = new List<string>
+            //{
+            //    "black",
+            //    "white",
+            //    "red",
+            //    "yellow",
+            //    "orange",
+            //    "blue",
+            //    "brown",
+            //    "purple",
+            //    "green",
+            //    "brown",
+            //    "gold",
+            //    "silver",
+            //};
+
+            //Console.WriteLine("Please enter a color (lower case).");
+            //string colorsInput = Console.ReadLine();
+            //bool colorGuess = false;
+
+            //for (int index = 0; index < colors.Count; index++)
+            //{
+            //    if (colors[index] == colorsInput)
+            //    {
+            //        colorGuess = true;
+            //        Console.WriteLine(index);
+            //    }
+            //}
+            //if (colorGuess == false)
+            //{
+            //    Console.WriteLine("Sorry, this color is not in the lidt.");
+            //}
+            //Console.ReadLine();
+           
+
+            // Part Six
+
+
+
+          
         }
     }
 }
