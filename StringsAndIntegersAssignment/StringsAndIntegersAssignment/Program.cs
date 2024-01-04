d1975593db5bf1fd49759c764450aaf49cfb72f7
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-04 01:50:10 -0800

    update StringsAndIntegerAssignment

--- StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -12,15 +12,40 @@ namespace StringsAndIntegersAssignment
         {
             List<int> listNumberFour = new List<int> { 1, 45, 38, 92, 90, 4, 76, 84, 33, 11, 32, 470, 857 };
 
-            Console.WriteLine("Please enter a number.");
-            int bill = Convert.ToInt32(Console.ReadLine());
-
-            foreach (int frank in listNumberFour)
+            try
             {
-                int wanda = frank / bill;
-                Console.WriteLine(wanda);
+                Console.WriteLine("Please enter a number.");
+                int bill = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int frank in listNumberFour)
+                {
+                    int wanda = frank / bill;
+                    Console.WriteLine(wanda);
+                }
+                Console.ReadLine();
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Try/Catch finished, you may continue with the program.");
             Console.ReadLine();
+            
+
+            
         }
     }
 }
