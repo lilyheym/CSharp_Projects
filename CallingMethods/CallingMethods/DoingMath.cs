60698060ce949d6d385174b20702ffffabd8a6ee
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-10 01:38:40 -0800

    Eh, progress I guess

---
+++ CallingMethods/CallingMethods/DoingMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class DoingMath
+    {
+        public DoingMath()
+        {
+
+        }
+        public static void DoingAddition()
+        {
+            
+            int methodOutput = userInput + 25;
+            Console.WriteLine(methodOutput);
+            Console.ReadLine();
+        }
+
+
+        public static void DoingSubtraction()
+        {
+            
+            int methodOutput2 = userInput2 - 5;
+            Console.WriteLine(methodOutput2);
+            Console.ReadLine();
+        }
+        public static void DoingMultiplication()
+        {
+            
+            int methodOutput3 = userInput3 * 3;
+            Console.WriteLine(methodOutput3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to do math operations on.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            DoingMath.DoingAddition();
+            DoingMath.DoingSubtraction();
+            DoingMath.DoingMultiplication();
+            
+        }
+    }
+}
