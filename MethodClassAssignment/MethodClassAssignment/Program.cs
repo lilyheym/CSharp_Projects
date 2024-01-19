d01135f916207d95b2de54050ad3cc44e88319cb
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-19 13:58:05 -0800

    Method class

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voyager obj = new Voyager();
+
+            int a = 1;
+            int b = 6;
+
+            int result = obj.Tuvok(a, b);
+
+            int result2 = obj.Tuvok(7, 5);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Voyager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Voyager
+    {
+        public void Tuvok(int a, int b = 0)
+        {
+            int c = a - 6;
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
