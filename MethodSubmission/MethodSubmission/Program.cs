4f7520cc7295f1c7c9398eba6c5b1148d452173d
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-19 03:50:11 -0800

    Method Submission

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StarTrek obj = new StarTrek();
+
+            // console requests user input two numbers, one at a time
+
+            Console.WriteLine("Please enter a number:");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number - optional:");
+            string forLooping = Console.ReadLine();
+            int userInput2;
+
+            // loop because compiler won't accept a null value otherwise
+
+            if (string.IsNullOrEmpty(forLooping))
+            {
+                userInput2 = 0;
+                
+            }
+            else
+            {
+                userInput2 = Convert.ToInt32(forLooping);
+                
+            }
+            int operationResult;
+
+            operationResult = obj.Enterprise(userInput1, userInput2);
+
+            Console.WriteLine(operationResult);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/StarTrek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class StarTrek
+    {
+        // can you tell I'm running out of names for all these math classes and methods?
+
+        public int Enterprise(int userInput1, [Optional]int userInput2)
+        {
+            int resultA = userInput1 + userInput2;
+
+
+            return resultA;
+        }
+    }
+}
