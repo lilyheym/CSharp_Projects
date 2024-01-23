a5a01a9b0d7e3004319c91118473b7700d432dd9
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-23 13:13:51 -0800

    MethodsAndObjects

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    // 3. Create another class called employee and have it inherit from the Person class. Give the employee class a property called 'ID' and have it be of data type int
+    public class Employee: Person
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4. Inside of the Main method, instatntiate and initialize an Employee object with a first name of 'Sample and a last name of 'Student' 
+
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                ID = 123  // Example ID
+            };
+
+            // 5. Call the superclass method SayName() on the Employee object
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
