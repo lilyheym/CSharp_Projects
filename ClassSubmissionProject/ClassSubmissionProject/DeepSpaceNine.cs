3d3ebfebc9b673aaccc39d0e7b366a1b75dc6719
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-22 16:36:46 -0800

    Finally

---
+++ ClassSubmissionProject/ClassSubmissionProject/DeepSpaceNine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionProject
+{
+    class DeepSpaceNine
+    {
+        // 1. Create a class with a void method that outputs an integer (dividing the data passed into it by two)
+        public void Defiant(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine($"Your number divided by 2 is: {result}");
+        }
+
+        // 4. Create a method with output paramaters
+        public void Quarks(int input, out int result)
+        {
+            result = input * 2;
+        }
+
+        // 5. Overload the method
+        public void Quarks(double input, out double result)
+        {
+            result = input * 5;
+        }
+
+        // 6. Declare a method to be static
+        public static int Morn(int Jadzia)
+        {
+            Jadzia = 7;
+            return Jadzia;
+        }
+    }
+}
---
+++ ClassSubmissionProject/ClassSubmissionProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Note to self: see how much easier this is to just put the assignment descriptors in the actual file instead of flipping back and forth?
+namespace ClassSubmissionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. instantiate the class
+            DeepSpaceNine obj = new DeepSpaceNine();
+
+            // 3. Have the user enter a number, call the method, and display the output
+            Console.WriteLine("Please enter a number to be divided by two.");
+
+            if(int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                obj.Defiant(userInput);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, input needs to be an integer.");
+            }
+            Console.ReadLine();
+
+            
+
+            
+        }
+    }
+}
