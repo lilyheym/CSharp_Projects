ea50b21a640fb5ac26fb90cad62cc0d8a4beb3de
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-23 16:16:29 -0800

    Abstract Class

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // 3. Create another class called Employee and have it inherit from the Person class
+    public class Employee : Person
+    {
+        // 4. Implement the SayName() method inside of the Employee class
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5. Inside the Main method, instantiate an Employee object with FirstName "Sample" and LastName "Student". Call the SayName() method on the object
+
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
