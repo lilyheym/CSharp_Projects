3b76cd99fb917d035d54f193af2c8ec617174734
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-26 01:51:47 -0800

    Operators

---
+++ OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    // 1. Create an Employee class with Id, FirstName, and LastName properties
+    class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // 2. In the Employee class, overload the "==" operator so it checks if two employee objects are equal by comparing their ID property. Remember that comparison operators must be done in pairs
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.ID == employee2.ID;
+        }
+        public static bool operator!=(Employee employee1, Employee employee2)
+        {
+            return !(employee1.ID == employee2.ID);
+        }
+    }
+}
---
+++ OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Program
+    {
+        // 3. In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their to their properties. Then compare the two Employee objects using the newly overloaded operators to display the results.
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee { ID = 1, FirstName = "Kassidy", LastName = "Yates" };
+            Employee employee2 = new Employee { ID = 2, LastName = "Benjamin", FirstName = "Sisko" };
+
+            if(employee1 == employee2)
+            {
+                Console.WriteLine("The employees have the same ID.");
+            }
+            else
+            {
+                Console.WriteLine("The employees don't have the same ID.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
