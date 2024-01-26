1cefc439ac94c6576ddde295298b8a52bf0b5590
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-25 21:24:22 -0800

    update

--- AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/Employee.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace AbstractClass
 {
     // 3. Create another class called Employee and have it inherit from the Person class
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
+
         // 4. Implement the SayName() method inside of the Employee class
 
         public override void SayName()
--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -10,22 +10,18 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
+            IQuittable quit = new Employee();
             // 5. Inside the Main method, instantiate an Employee object with FirstName "Sample" and LastName "Student". Call the SayName() method on the object
 
-            //Employee employee = new Employee
-            //{
-            //    FirstName = "Sample",
-            //    LastName = "Student"
-            //};
-            //employee.SayName();
-            //Console.ReadLine();
-
-            Employee secondEmployee = new Employee()
+            Employee employee = new Employee
             {
-                FirstName = "Second",
+                FirstName = "Sample",
                 LastName = "Student"
             };
-            secondEmployee.Quit();
+            employee.SayName();
+            Console.ReadLine();
+
+            quit.Quit();
             Console.ReadLine();
         }
     }
