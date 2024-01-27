0fb33ce020164519d4dc1c7eddded75fa2eba760
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-01-26 17:30:03 -0800

    Parameters Assignment

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // 1. Make the Employee Class take a generic type parameter
+    public class Employee<T>
+    {
+        // 2. Add a property to the Employee class called "things" and have its data type be a generic list matching the generic type of the class
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<T> Things { get; set; }
+
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Instantiate an Employee object with type "string" as its generic parameter. Assign a list of strings as the property value of Things
+
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.Things = new List<string> { "one", "two", "three" };
+
+            // 4. Instantitate an Employee object with type "int" as its generic parameter. Assign a list of integers as the property value of Things
+
+            Employee<int> employeeInteger = new Employee<int>();
+            employeeInteger.Things = new List<int> { 1, 2, 3 };
+
+            // 5. Create a loop that prints all of the Things to the console
+
+            foreach(var thing in employeeString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach(var thing in employeeInteger.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
