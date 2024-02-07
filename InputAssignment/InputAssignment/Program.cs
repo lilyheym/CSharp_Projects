72eb8a74b5a180f62c498aeac8708235f8f32510
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-02-07 03:52:29 -0800

    input assignment

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number.");
+            string inputText = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\lilyr\Logs\InpuAssignment.txt", inputText);
+            string readInput = File.ReadAllText(@"C:\Users\lilyr\Logs\InpuAssignment.txt");
+            Console.WriteLine(readInput);
+            Console.Read();
+        }
+    }
+}
