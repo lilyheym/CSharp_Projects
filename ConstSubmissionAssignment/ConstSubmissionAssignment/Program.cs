b45b6cd05a87f37ca863e95dc2511af3d4440ff1
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-02-08 23:50:32 -0800

    update

--- ConstSubmissionAssignment/ConstSubmissionAssignment/Program.cs
+++ ConstSubmissionAssignment/ConstSubmissionAssignment/Program.cs
@@ -10,6 +10,14 @@ namespace ConstSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            // 1. Create a const variable
+            const string Example = "This is an example of a const.";
+
+            // 2. Create a variable using the keyword "var".
+            var anotherExample = "And this is an example of a var.";
+
+          
         }
+
     }
 }
---
+++ ConstSubmissionAssignment/ConstSubmissionAssignment/exampleClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstSubmissionAssignment
+{
+    // 3. Chain two constructors together
+    public class exampleClass
+    {
+        private int exampleInt;
+
+        public exampleClass(int example)
+        {
+            exampleInt = example;
+        }
+
+        public exampleClass(int example, int example2)
+        {
+            exampleInt = example + example2;
+        }
+    }
+}
